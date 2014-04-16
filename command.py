f06d9c6086dcba2dfb77fed0e1e28357965923b9
arthurnum <kivy@kivy-VirtualBox.(none)>
2014-04-16 08:26:27 -0500

    Run commands

--- command.py
+++ command.py
@@ -1,14 +1,20 @@
 __author__ = 'arthurnum'
 
 
+from kivy.animation import Animation
+
+
 class Command:
     pass
 
 
 class CommandMove:
 
-    def do(self):
-        pass
+    def do(self, voloda, cells):
+        voloda.cell_x += 1
+        dest_cell = cells[voloda.cell_x][voloda.cell_y]
+        anim = Animation(x=dest_cell.x, y=dest_cell.y, duration=0.5)
+        anim.start(voloda)
 
 
 class CommandWait:
