a3904ca250d8933c081eaac27a6a0fb73c4b064c
arthurnum <kivy@kivy-VirtualBox.(none)>
2014-04-16 08:26:27 -0500

    Run commands

--- main_layout.py
+++ main_layout.py
@@ -7,19 +7,24 @@ from kivy.properties import ObjectProperty
 from kivy.uix.popup import Popup
 from kivy.uix.button import Button
 from functools import partial
+from command import CommandMove
 
 
 cells = []
+commands = []
+
 
 class MainLayout(BoxLayout):
     cell_grid = ObjectProperty()
     voloda = ObjectProperty()
 
     def make(self):
+        commands.append(CommandMove())
+        commands.append(CommandMove())
         for x in range(5):
             cells.append([])
             for y in range(5):
-                cell = Cell(pos_hint={'x': y * 0.2, 'y': x * 0.2})
+                cell = Cell(pos_hint={'x': x * 0.2, 'y': y * 0.2})
                 cells[x].append(cell)
                 self.cell_grid.add_widget(cell)
         self.voloda = Voloda()
@@ -27,7 +32,7 @@ class MainLayout(BoxLayout):
 
     def open_command_popup(self):
         box = BoxLayout(orientation='vertical')
-        box.add_widget(Button(text='hello', on_press=partial(self.define_button, command='huita')))
+        box.add_widget(Button(text='hello', on_press=self.define_button))
         box.add_widget(Button(text='hello', on_press=self.define_button))
         box.add_widget(Button(text='hello', on_press=self.define_button))
         box.add_widget(Button(text='hello', on_press=self.define_button))
@@ -38,6 +43,10 @@ class MainLayout(BoxLayout):
         popup = Popup(title='Commands', content=box)
         popup.size_hint = (0.2, 0.8)
         popup.open()
+
+    def run(self):
+        for command in commands:
+            command.do(self.voloda, cells)
 
     def define_button(self, *args):
         print args
